4ae2261d28581b8b773664bf80de21dc32134e7e
dangam111 <hyeminnie486@gmail.com>
2025-10-08 09:43:23 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 기존 코드: st.title("My First App")
+# 수정 코드:
+st.title("🏫 박혜민 소개하기")
+st.write("안녕하세요 매원중학교 교사 박혜민입니다!")
